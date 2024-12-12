acbb503fb6c94adbf816d314ddab6b45d7bd3c80
muab01 <mustafasis1995@gmail.com>
2024-12-12 16:20:08 +0100

    Add order controller and order routes

---
+++ backend/controllers/orderController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { pool } from '../db/db';
+
+const orderController = {
+ 
+  createOrder: async (req: Request, res: Response) => {
+    const { customer_name, customer_contact, pickup_or_dine_in, total_price, items } = req.body;
+    try {
+      const orderResult = await pool.query(
+        'INSERT INTO Orders (customer_name, customer_contact, pickup_or_dine_in, total_price) VALUES ($1, $2, $3, $4) RETURNING id',
+        [customer_name, customer_contact, pickup_or_dine_in, total_price]
+      );
+
+      const orderId = orderResult.rows[0].id;
+
+      for (const item of items) {
+        await pool.query(
+          'INSERT INTO OrderItems (order_id, menu_id, quantity, price) VALUES ($1, $2, $3, $4)',
+          [orderId, item.menu_id, item.quantity, item.price]
+        );
+      }
+
+      res.status(201).json({ message: 'Order created successfully', orderId });
+    } catch (error) {
+      console.error('Error creating order:', error);
+      res.status(500).json({ error: 'Failed to create order.' });
+    }
+  },
+}
+
+export default orderController;
