18886ae77d569d33b866749f27560b94d0ebb88c
muab01 <mustafasis1995@gmail.com>
2024-12-17 23:52:50 +0100

    Add routes.tsx file

---
+++ frontend/src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+/* import Header from './components/Header'; */
+import Home from '../pages/Home';
+import Menu from '../components/Menu';
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+   {/*    <Header /> */}
+    
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    
+  );
+};
+
+export default App;
