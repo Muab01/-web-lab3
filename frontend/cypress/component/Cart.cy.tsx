73a78ae2c51c8abfa265d9fb704cbce17ab1fa07
muab01 <mustafasis1995@gmail.com>
2024-12-20 01:42:35 +0100

    Add initial test for Cart component

--- frontend/cypress/component/Cart.cy.tsx
+++ frontend/cypress/component/Cart.cy.tsx
@@ -1,13 +1,13 @@
 import Cart from '../../src/components/Cart';
+import { mount } from 'cypress/react18';
 
 describe('Cart Component', () => {
-  it('should display the correct number of items & total price when adding an item', () => {
+  it('should display the Cart ID', () => {
+    // Given: Jag har en kundvagn med ett specifikt ID
     const cartId = 1;
-    cy.mount(<Cart cartId={cartId} />);
+    mount(<Cart cartId={cartId} />);
 
-    cy.contains('Add to Cart').click();
-
-    cy.contains('1 pasta/dish in cart').should('be.visible');
-    cy.contains('Total: $10.00').should('be.visible');
+    // Then: Komponentens text ska visa Cart ID
+    cy.contains('Cart ID: 1').should('be.visible');
   });
 });
