a8149146e9d8caee98bda2814df2d86d2a4f0064
muab01 <mustafasis1995@gmail.com>
2024-12-20 01:43:48 +0100

    Add Cart component to pass the test

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,14 +1,9 @@
 interface CartProps {
-    cartId: number;
-  }
-  
-  const Cart: React.FC<CartProps> = ({ cartId }) => {
-    return (
-      <div>
-        <p>Cart ID: {cartId}</p>
-      </div>
-    )
-  }
-  
-  export default Cart;
-  
+  cartId: number;
+}
+
+const Cart: React.FC<CartProps> = ({ cartId }) => {
+  return <p>Cart ID: {cartId}</p>;
+};
+
+export default Cart;
