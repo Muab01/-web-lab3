c765d55a91557444cf6ef23b04099f1d1b97ce09
muab01 <mustafasis1995@gmail.com>
2024-12-20 23:23:27 +0100

    Test Cart using TDD and BDD

--- frontend/cypress/component/Cart.cy.tsx
+++ frontend/cypress/component/Cart.cy.tsx
@@ -1,16 +1,20 @@
 import Cart from '../../src/components/Cart';
 import { mount } from 'cypress/react18';
 
-describe('Cart Component', () => {
-    it('should display 1 item in cart when a pasta dish is added', () => {
-        // Given: En cart/kundvagn
-        const cartId = 1;
-        mount(<Cart cartId={cartId} />);
-      
-        // When: Vi lägger till en vara
-        cy.contains('Add to Cart').click();
-      
-        // Then: Texten ska visa att det finns 1 vara i kundvagnen
-        cy.contains('1 pasta/dish in cart').should('be.visible');
-    });
-})      
+describe('Kundvagnskomponent', () => {
+  it('ska visa att en maträtt läggs till i kundvagnen', () => {
+    const cartId = 1;
+
+    // Given: Menyn är laddad och carts/kundvagnen är tom
+    cy.intercept('GET', '/api/menu', { fixture: 'menu.json' }).as('hämtaMeny');
+    mount(<Cart cartId={cartId} />);
+    cy.wait('@hämtaMeny');
+    cy.get('[data-cy="cart-empty"]').should('be.visible');
+
+    // When: Jag klickar på knappen för att lägga till en maträtt i carts/kundvagnen
+    cy.get('[data-cy="add-to-cart"]').should('be.visible').click();
+
+    // Then: Maträtten visas i carts/kundvagnen med ett meddelande
+    cy.get('[data-cy="cart-item"]').should('contain', 'har lagts till i kundvagnen');
+  });
+});
