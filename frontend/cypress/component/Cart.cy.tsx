4d10d04155de64dc88f74e38749adafb5d29ec9f
muab01 <mustafasis1995@gmail.com>
2024-12-20 02:05:07 +0100

    Second test for displaying 'Cart is empty' with no items

--- frontend/cypress/component/Cart.cy.tsx
+++ frontend/cypress/component/Cart.cy.tsx
@@ -2,12 +2,12 @@ import Cart from '../../src/components/Cart';
 import { mount } from 'cypress/react18';
 
 describe('Cart Component', () => {
-  it('should display the Cart ID', () => {
-    // Given: Jag har en kundvagn med ett specifikt ID
-    const cartId = 1;
-    mount(<Cart cartId={cartId} />);
-
-    // Then: Komponentens text ska visa Cart ID
-    cy.contains('Cart ID: 1').should('be.visible');
-  });
-});
+it('should display "Cart is empty" when there are no items', () => {
+     // Given: En cart/kundvagn utan några varor
+        const cartId = 1;
+        mount(<Cart cartId={cartId} />);
+      
+    // Then: Texten "Cart is empty" ska visas
+        cy.contains('Cart is empty').should('be.visible');
+    });
+})      
